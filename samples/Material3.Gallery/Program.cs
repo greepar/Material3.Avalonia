730c1c2566bad189d7967bb3ec5d44153566541c
greepar <greepar@outlook.com>
2026-08-14 04:58:29 +0800

    Unify NativeAOT and WASM release artifacts

--- samples/Material3.Gallery/Program.cs
+++ samples/Material3.Gallery/Program.cs
@@ -12,7 +12,48 @@ internal static class Program
         BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
     }
 
-    public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>()
+    public static AppBuilder BuildAvaloniaApp()
+    {
+        var builder = AppBuilder.Configure<App>()
             .UsePlatformDetect()
             .WithInterFont();
+
+        if (OperatingSystem.IsWindows())
+        {
+            builder = builder.With(new Win32PlatformOptions
+            {
+                RenderingMode =
+                [
+                    Win32RenderingMode.AngleEgl,
+                    Win32RenderingMode.Software,
+                ],
+            });
+        }
+
+        if (OperatingSystem.IsLinux())
+        {
+            builder = builder.With(new X11PlatformOptions
+            {
+                RenderingMode =
+                [
+                    X11RenderingMode.Glx,
+                    X11RenderingMode.Software,
+                ],
+            });
+        }
+
+        if (OperatingSystem.IsMacOS())
+        {
+            builder = builder.With(new AvaloniaNativePlatformOptions
+            {
+                RenderingMode =
+                [
+                    AvaloniaNativeRenderingMode.OpenGl,
+                    AvaloniaNativeRenderingMode.Software,
+                ],
+            });
+        }
+
+        return builder;
+    }
 }
